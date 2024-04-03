946100f8d38093d3724c94d1e2cc826e0d6ab6aa
Francesco <sr1163374794@live.com>
2024-04-03 16:38:47 +0200

    Integrazione sistema di Login e Logout funzionante

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Capstone.Controllers
+{
+    public class AuthController : Controller
+    {
+        DBContext db = new DBContext();
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Users user)
+        {
+            var loggedUser = db.Users.Where(u=> u.Email == user.Email && u.Password == user.Password).FirstOrDefault();
+
+            if (loggedUser != null)
+            {
+                FormsAuthentication.SetAuthCookie(loggedUser.UserId.ToString(), true);
+                return RedirectToAction("Index", "Home");
+            }
+
+            TempData["ErrorLogin"] = true;
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
